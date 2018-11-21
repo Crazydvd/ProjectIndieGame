e38ed67fa2df795122914b098550985908b36fc9
LAPTOP-DANNY\Danny <dvduist@hotmail.nl>
2018-11-21 22:52:57 +0100

    Changed code (fixing the movement is ez now) Tried to get bouncing to work, so far little success.

--- ProjectIndieGame/Assets/Scripts/Movement.cs
+++ ProjectIndieGame/Assets/Scripts/Movement.cs
@@ -6,14 +6,17 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody _rigidBody;
 
-    private float _speed = 0.2f;
+    [SerializeField] private float _speed = 0.2f;
 
     private Vector3 forward;
     private Vector3 backward;
     private Vector3 left;
     private Vector3 right;
 
-    // Use this for initialization
+    private Vec2 _velocity;
+    private Vec2 _normal;
+
+
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
@@ -21,30 +24,67 @@ public class Movement : MonoBehaviour
         forward = new Vector3(0, 0, _speed);
         backward = new Vector3(0, 0, -_speed);
         left = new Vector3(-_speed, 0, 0);
-        forward = new Vector3(_speed, 0, 0);
+        right = new Vector3(_speed, 0, 0);
+
+        _velocity = Vec2.zero;
+        _normal = Vec2.zero;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            _rigidBody.velocity += forward;
+            _rigidBody.AddForce(forward, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            _rigidBody.velocity += backward;
+            _rigidBody.AddForce(backward, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            _rigidBody.velocity += left;
+            _rigidBody.AddForce(left, ForceMode.VelocityChange);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            _rigidBody.velocity += right;
+            _rigidBody.AddForce(right, ForceMode.VelocityChange);
+        }
+
+        if (_rigidBody.velocity.magnitude > _speed)
+        {
+            _rigidBody.velocity = _rigidBody.velocity.normalized;
+            _rigidBody.velocity = _rigidBody.velocity * _speed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity.SetXY(_rigidBody.velocity.x, _rigidBody.velocity.z);
+    }
+
+    private void LateUpdate()
+    {
+        //_rigidBody.velocity *= 0;
+    }
+
+    private void reflect(Vector3 pNormal)
+    {
+        _normal.SetXY(pNormal.x, pNormal.z);
+
+        _velocity.Reflect(_normal, 1);
+        _rigidBody.velocity.Set(_velocity.x, 0, _velocity.y);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.ToUpper() == "WALL")
+        {
+            print("Before: " + _rigidBody.velocity);
+            reflect(collision.contacts[0].normal);
+            print("After: " + _rigidBody.velocity);
+            print(" ");
         }
     }
 }
---
+++ ProjectIndieGame/Assets/Scripts/Physics/Vec2.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vec2
+{
+    public static Vec2 zero { get { return new Vec2(0, 0); } }
+
+    public float x = 0;
+    public float y = 0;
+
+    private static System.Random _random = new System.Random();
+
+    public Vec2(float pX = 0, float pY = 0)
+    {
+        x = pX;
+        y = pY;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("({0}, {1})", x, y);
+    }
+
+    public Vec2 Add(Vec2 other)
+    {
+        x += other.x;
+        y += other.y;
+        return this;
+    }
+
+    public Vec2 Subtract(Vec2 other)
+    {
+        x -= other.x;
+        y -= other.y;
+        return this;
+    }
+
+    public Vec2 Scale(float pScalar)
+    {
+        x *= pScalar;
+        y *= pScalar;
+        return this;
+    }
+
+    public float Length()
+    {
+        //x² + y² = lVec2²      ²root(x² + y²) = lVec2
+        return Mathf.Sqrt(x * x + y * y);
+    }
+
+    public Vec2 Normalize()
+    {
+        if (Length() != 0)
+        {
+            Scale(1 / Length());
+        }
+        return this;
+    }
+
+    public Vec2 Clone()
+    {
+        return new Vec2(x, y);
+    }
+
+    public void SetXY(float pX, float pY)
+    {
+        x = pX;
+        y = pY;
+    }
+
+    public void SetXY(Vec2 pVec2)
+    {
+        x = pVec2.x;
+        y = pVec2.y;
+    }
+
+    public static float Deg2Rad(float pDegrees)
+    {
+        return (Mathf.PI / 180) * pDegrees;
+    }
+
+    public static float Rad2Deg(float pRadians)
+    {
+        return (180 / Mathf.PI) * pRadians;
+    }
+
+    public static Vec2 GetUnitVectorDegrees(float pDegrees)
+    {
+        float radians = Deg2Rad(pDegrees);
+
+        return new Vec2(Mathf.Cos(radians), Mathf.Sin(radians));
+    }
+
+    public static Vec2 GetUnitVectorRadians(float pRadians)
+    {
+        return new Vec2(Mathf.Cos(pRadians), Mathf.Sin(pRadians));
+    }
+
+    public static Vec2 RandomUnitVector()
+    {
+        float direction = _random.Next(0, 360);
+        return GetUnitVectorDegrees(direction);
+    }
+
+    public Vec2 SetAngleDegrees(float pDegrees)
+    {
+        float radians = Deg2Rad(pDegrees);
+        SetXY(Mathf.Cos(radians) * Length(), Mathf.Sin(radians) * Length());
+        return this;
+    }
+
+    public Vec2 SetAngleRadians(float pRadians)
+    {
+        SetXY(Mathf.Cos(pRadians) * Length(), Mathf.Sin(pRadians) * Length());
+        return this;
+    }
+
+    public float GetAngleRadians()
+    {
+        return Mathf.Atan2(y, x);
+    }
+
+    public float GetAngleDegrees()
+    {
+        float radians = Mathf.Atan2(y, x);
+        return Rad2Deg(radians);
+    }
+
+    public void RotateDegrees(float pDegrees)
+    {
+        float radians = Deg2Rad(pDegrees);
+        SetXY(x * Mathf.Cos(radians) - y * Mathf.Sin(radians), x * Mathf.Sin(radians) + y * Mathf.Cos(radians));
+    }
+
+    public void RotateRadians(float pRadians)
+    {
+        SetXY(x * Mathf.Cos(pRadians) - y * Mathf.Sin(pRadians), x * Mathf.Sin(pRadians) + y * Mathf.Cos(pRadians));
+    }
+
+    public void RotateAroundDegrees(float pPointX, float pPointY, float pDegrees)
+    {
+        //float radians = Deg2Rad(pDegrees);
+        //SetXY(((x - pPointX) * Mathf.Cos(radians) - (y - pPointY) * Mathf.Sin(radians)) + pPointX, ((x - pPointX) * Mathf.Sin(radians) + (y - pPointY) * Mathf.Cos(radians)) + pPointY);
+
+        x -= pPointX;
+        y -= pPointY;
+        RotateDegrees(pDegrees);
+        x += pPointX;
+        y += pPointY;
+    }
+
+    public void RotateAroundRadians(float pPointX, float pPointY, float pRadians)
+    {
+        //SetXY(((x - pPointX) * Mathf.Cos(pRadians) - (y - pPointY) * Mathf.Sin(pRadians)) + pPointX, ((x - pPointX) * Mathf.Sin(pRadians) + (y - pPointY) * Mathf.Cos(pRadians)) + pPointY);
+
+        x -= pPointX;
+        y -= pPointY;
+        RotateRadians(pRadians);
+        x += pPointX;
+        y += pPointY;
+    }
+
+    public Vec2 Normal()
+    {
+        Vec2 vec2 = new Vec2(-y, x);
+        return vec2.Normalize();
+    }
+
+    public float Dot(Vec2 pVec2)
+    {
+        return x * pVec2.x + y * pVec2.y;
+    }
+
+    public Vec2 Reflect(Vec2 pNormal, float pBounciness = 1)
+    {
+        Subtract(pNormal.Clone().Scale((1 + pBounciness) * Dot(pNormal)));
+        return this;
+    }
+}
