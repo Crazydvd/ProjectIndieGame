6bb570009a1a8d4d7afdcd9b8f496607d5a86d35
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-11-21 13:01:20 +0100

    Shake that ass

--- ProjectIndieGame/Assets/Movement.cs
+++ ProjectIndieGame/Assets/Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float speed;
+
     // Use this for initialization
     void Start()
     {
@@ -15,22 +17,22 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, 0, 1);
+            transform.position += new Vector3(0, 0, speed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, 0, -1);
+            transform.position += new Vector3(0, 0, -speed);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-1, 0, 0);
+            transform.position += new Vector3(-speed, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(1, 0, 0);
+            transform.position += new Vector3(speed, 0, 0);
         }
     }
 }
