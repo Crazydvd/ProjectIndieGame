51e7c480824a72b41d21f7b4667b5f3c3b0c3c8b
Luuk Oude Vrielink <l-oudevrielink@hotmail.com>
2018-11-22 12:17:03 +0100

    Main menu prototype done

---
+++ ProjectIndieGame/Assets/Scripts/CharacterSelectWindowScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectWindowScript : MonoBehaviour {
+
+    [SerializeField] GameObject _mainMenu;
+    [SerializeField] GameObject _stageSelect;
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("Char_P1", -1);
+        PlayerPrefs.SetInt("Char_P2", -1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(PlayerPrefs.GetInt("Char_P1") != -1 && PlayerPrefs.GetInt("Char_P2") != -1)
+        {
+            _stageSelect.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        else if (Input.GetButtonDown("Decline_P1") && PlayerPrefs.GetInt("Char_P1") == -1)
+        {
+            _mainMenu.SetActive(true);
+            gameObject.SetActive(false);
+        }
+	}
+}
