5dfac2a11630607cef781228e1ce702bc8409e43
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-11-22 14:05:58 +0100

    Counting lives and res screen

--- ProjectIndieGame/Assets/Scripts/DeathScript.cs
+++ ProjectIndieGame/Assets/Scripts/DeathScript.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeathScript : MonoBehaviour
 {
+    public Image resolutionScreen;
     Rigidbody _rigidbody;
     Vector3 startPosition;
-    Vector3 zero;
+    PlayerStatus _playerStatus;    
 
     // Use this for initialization
     void Start()
     {
         startPosition = transform.position;
         _rigidbody = GetComponent<Rigidbody>();
-        zero = new Vector3(0, 0, 0);
+        _playerStatus = GetComponent<PlayerStatus>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -21,8 +23,17 @@ public class DeathScript : MonoBehaviour
         if (other.tag.ToUpper() == "BOUNDARY")
         {
             Debug.Log("boop");
-            _rigidbody.velocity = zero;
-            this.transform.position = startPosition;
+            _playerStatus.DecreaseLives();
+
+            if (_playerStatus.GetLives() <= 0)
+            {
+                Debug.Log("You just fucking died");
+                resolutionScreen.gameObject.SetActive(true);
+                Destroy(gameObject, 0f);
+                return;
+            }
+            _rigidbody.velocity = Vector3.zero;
+            transform.position = startPosition;
         }
     }
 }
---
+++ ProjectIndieGame/Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public Text livesUI;
+
+    int damage = 0;
+    int lives = 3;
+
+    void Start()
+    {
+        livesUI.text = livesUI.text + lives;
+    }
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public void IncreaseDamage(int pAmmount)
+    {
+        damage += pAmmount;
+    }
+
+    public int GetLives()
+    {
+        return lives;
+    } 
+
+    public void DecreaseLives()
+    {
+        lives--;
+        livesUI.text = livesUI.text.Remove(livesUI.text.Length - 1) + lives;
+    }
+}
