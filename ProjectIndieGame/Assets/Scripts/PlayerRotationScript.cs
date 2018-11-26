892c5109abb6995a26c101c7e631d8e27c1ae54f
Luuk Oude Vrielink <l-oudevrielink@hotmail.com>
2018-11-26 15:17:21 +0100

    stop rolling you fucker

--- ProjectIndieGame/Assets/Scripts/PlayerRotationScript.cs
+++ ProjectIndieGame/Assets/Scripts/PlayerRotationScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerRotationScript : MonoBehaviour {
 
-    Rigidbody _rigidBody;
+    [SerializeField] private float _stopRotationLimit = 0.1f;
+    private Rigidbody _rigidBody;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,13 @@ public class PlayerRotationScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Quaternion.AngleAxis(90, Vector3.up) * _rigidBody.velocity, Space.Self);
+        if(_rigidBody.velocity.magnitude < _stopRotationLimit)
+        {
+            _rigidBody.freezeRotation = true;
+        }
+        else
+        {
+            _rigidBody.freezeRotation = false;
+        }
 	}
 }
