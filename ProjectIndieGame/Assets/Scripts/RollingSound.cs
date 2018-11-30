1da1821a54814363857f1d39a130f2d344ebb59e
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-11-30 15:06:37 +0100

    Added timer, and a little sound fix

--- ProjectIndieGame/Assets/Scripts/RollingSound.cs
+++ ProjectIndieGame/Assets/Scripts/RollingSound.cs
@@ -9,17 +9,16 @@ public class RollingSound : MonoBehaviour
 
     Rigidbody _rigidbody;
 
-    void Awake()
+    private bool _paused;
+
+    void Start()
     {
         _rollSound = FMODUnity.RuntimeManager.CreateInstance("event:/rolling");
         _rollSound.getParameter("speed", out _speed);
         _rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void Start()
-    {
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(_rollSound, GetComponent<Transform>(), GetComponent<Rigidbody>());
         _rollSound.start();
+        _rollSound.release();
     }
 
 
@@ -28,5 +27,24 @@ public class RollingSound : MonoBehaviour
         float _speedParameter = _rigidbody.velocity.magnitude / 100f;
         
         _speed.setValue(_speedParameter);
+
+        if (Time.timeScale == 0)
+        {
+            _rollSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            _paused = true;
+        }
+        else if (_paused)
+        {
+            _rollSound = FMODUnity.RuntimeManager.CreateInstance("event:/rolling");
+            _rollSound.getParameter("speed", out _speed);
+            _rollSound.start();
+            _rollSound.release();
+            _paused = false;
+        }
+    }
+
+    void OnDestroy()
+    {
+        _rollSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 }
---
+++ ProjectIndieGame/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] private float _givenTime = 180;
+
+    private Text _timerUI;
+    private PlayersHandler _playerHandler;
+
+    void Start()
+    {
+        _timerUI = GetComponent<Text>();
+        _playerHandler = Camera.main.GetComponent<PlayersHandler>();
+        _timerUI.text = ((int)_givenTime).ToString();
+    }
+    
+    void Update()
+    {
+        if (_givenTime <= 0)
+        {
+            string winner = "No one";
+            PlayerStatus bestPlayerStats = null;
+
+            foreach (GameObject player in _playerHandler.GetPlayers())
+            {
+                PlayerStatus playerStats = player.GetComponentInChildren<PlayerStatus>();
+
+                if (bestPlayerStats == null)
+                {
+                    bestPlayerStats = playerStats;
+                    winner = player.name;
+                    continue;
+                }
+
+                if (playerStats.GetLives() > bestPlayerStats.GetLives())
+                {
+                    bestPlayerStats = playerStats;
+                    winner = player.name;
+                }
+                else if(playerStats.GetLives() == bestPlayerStats.GetLives())
+                {
+                    if (playerStats.GetDamage() < bestPlayerStats.GetDamage())
+                    {
+                        bestPlayerStats = playerStats;
+                        winner = player.name;
+                    }
+                    else if (playerStats.GetDamage() == bestPlayerStats.GetDamage())
+                    {
+                        winner = "No one";
+                    }
+                }
+            }
+
+            _playerHandler.EndGame(winner);
+            Destroy(this, 0);
+        }
+
+        _givenTime -= Time.deltaTime;
+        _timerUI.text = ((int)_givenTime).ToString(); 
+    }
+}
