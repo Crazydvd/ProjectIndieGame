3e932ee9e4268fabc4f94615e861dd303efe3c72
MY-LITTLE-CHAD\Jesse <dron20d@mail.ru>
2018-12-05 14:52:16 +0100

    Sound fix for getting hit and transparency fix

---
+++ ProjectIndieGame/Assets/Scripts/ChangeRenderMode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ChangeRenderMode
+{
+    /// <summary>
+    /// 0 - Opaque. 1 - Transparent.
+    /// </summary>
+    /// <param name="pMaterial"></param>
+    /// <param name="pMode"></param>
+    public static void ChangeMode(Material pMaterial, int pMode)
+    {
+        switch (pMode)
+        {
+            case 0:
+                pMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                pMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                pMaterial.SetInt("_ZWrite", 1);
+                pMaterial.DisableKeyword("_ALPHATEST_ON");
+                pMaterial.DisableKeyword("_ALPHABLEND_ON");
+                pMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                pMaterial.renderQueue = -1;
+                break;
+            case 1:
+                pMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                pMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                pMaterial.SetInt("_ZWrite", 0);
+                pMaterial.DisableKeyword("_ALPHATEST_ON");
+                pMaterial.DisableKeyword("_ALPHABLEND_ON");
+                pMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                pMaterial.renderQueue = 3000;
+                break;
+            default:
+                pMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                pMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                pMaterial.SetInt("_ZWrite", 1);
+                pMaterial.DisableKeyword("_ALPHATEST_ON");
+                pMaterial.DisableKeyword("_ALPHABLEND_ON");
+                pMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                pMaterial.renderQueue = -1;
+                break;
+        }
+    }
+}
